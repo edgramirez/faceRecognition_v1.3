2294cc9b6a0bb0cdf5c4d6722f461c73ca039447
Edgar Ramirez <edgcramirez@gmail.com>
2021-06-19 12:15:57 -0500

    Only find project

---
+++ apps/deepstream-imagedata-multistream/subject_run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#rm -f data/video_encoded_faces/*
+rm -f data/encoded_known_faces/*
+rm -f data/found_faces/*
+
+#ls data/found_faces/
+#./faces.py loadFaces
+#ls -lartc data/found_faces/
+
+#cp data/encoded_known_faces/knownFaces.dat /tmp/data/encoded_known_faces/
+#ls -lartc data/encoded_known_faces/knownFaces.dat
+
+#rm -rf /tmp/stream_0; python3 face_detection.py     file:///tmp/obama_biden.mp4     /tmp/stream_0
+rm -rf /tmp/stream_0; python3 subject_search.py     file:///tmp/amlo.mp4            /tmp/stream_0
+#rm -rf /tmp/stream_0; python3 face_detection.py     file:///tmp/HD_CCTV_Camera.mp4  /tmp/stream_0
+
+exit 111
+ssh edgar@192.168.130.5 'rm -rf /tmp/stream_0'
+ssh edgar@192.168.130.5 'rm -f /tmp/resultados/*'
+
+scp -r /tmp/stream_0         edgar@192.168.130.5:/tmp
+scp    /tmp/found_elements/* edgar@192.168.130.5:/tmp/resultados/
