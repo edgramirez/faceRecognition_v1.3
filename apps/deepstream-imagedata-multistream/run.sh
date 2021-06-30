cfed9f0c27a46cac0849acf094a62854d9d0549c
edgramirez <edgcramirez@gmail.com>
2021-06-30 23:27:43 +0000

    running with black list

--- apps/deepstream-imagedata-multistream/run.sh
+++ apps/deepstream-imagedata-multistream/run.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 #rm -f data/video_encoded_faces/*
 
-./load_subject.py loadFaces
+./load_subject.py blacklist
 
 #cp data/encoded_known_faces/knownFaces.dat /tmp/data/encoded_known_faces/
 #ls -lartc data/encoded_known_faces/knownFaces.dat
 
 #rm -rf /tmp/stream_0; python3 face_detection.py     file:///tmp/obama_biden.mp4     /tmp/stream_0
+#rm -rf /tmp/stream_0; python3 subject_search.py     rtsp://192.168.128.6:9000/live /tmp/stream_0
+#rm -rf /tmp/stream_0; python3 face_detection.py     file:///tmp/obama_biden.mp4            /tmp/stream_0
 rm -rf /tmp/stream_0; python3 face_detection.py     file:///tmp/amlo.mp4            /tmp/stream_0
 #rm -rf /tmp/stream_0; python3 face_detection.py     file:///tmp/HD_CCTV_Camera.mp4  /tmp/stream_0
 
