b6b2c7b8a1238a41181a0149dc56abf4efe1ed81
Edgar Ramirez <edgcramirez@gmail.com>
2021-10-19 20:05:03 -0400

    test config

---
+++ apps/deepstream-imagedata-multistream/configs/Server_Emulatation_configs_from_Excel.py
@@ -0,0 +1,46 @@
+{ 
+    "FA:KE:4b:8d:49:68" : {
+        "service1": {
+            "serviceType": "find",
+            "source": "file:///tmp/amlo.mp4",
+            "enabled": true,
+            "faceDbFile": "/home/edgar/faceRecognition/",
+            "checkBlackList": true,
+            "checkWhieteList": true,
+            "ignorePreviousDb": true,
+            "saveFacesDb": true
+            },
+        "service2": {
+            "serviceType": "find",
+            "source": "file:///tmp/amlo.mp4",
+            "enabled": true,
+            "faceDbFile": "/home/edgar/faceRecognition/",
+            "checkBlackList": true,
+            "checkWhieteList": true,
+            "ignorePreviousDb": true,
+            "saveFacesDb": true
+            }
+        },
+    "00:04:4b:8d:49:68" : {
+        "service1": {
+            "serviceType": "find",
+            "source": "file:///tmp/amlo.mp4",
+            "enabled": true,
+            "faceDbFile": "/home/edgar/faceRecognition/",
+            "checkBlackList": true,
+            "checkWhieteList": true,
+            "ignorePreviousDb": true,
+            "saveFacesDb": true
+            },
+        "service2": {
+            "serviceType": "find",
+            "source": "file:///tmp/amlo.mp4",
+            "enabled": true,
+            "faceDbFile": "/home/edgar/faceRecognition/",
+            "checkBlackList": true,
+            "checkWhieteList": true,
+            "ignorePreviousDb": true,
+            "saveFacesDb": true
+            }
+        }
+}
