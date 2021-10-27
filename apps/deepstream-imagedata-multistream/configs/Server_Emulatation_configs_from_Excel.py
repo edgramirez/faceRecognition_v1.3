7c111f24841ffeb80e4347ab0f24e400cc1a9dee
Edgar Ramirez <edgcramirez@gmail.com>
2021-10-27 18:45:03 -0400

    testing config

--- apps/deepstream-imagedata-multistream/configs/Server_Emulatation_configs_from_Excel.py
+++ apps/deepstream-imagedata-multistream/configs/Server_Emulatation_configs_from_Excel.py
@@ -3,6 +3,7 @@
         "service1": {
             "serviceType": "find",
             "source": "file:///tmp/amlo.mp4",
+            "cameraId": "camera1_mac_address",
             "enabled": true,
             "faceDbFile": "found_service1.dat",
             "checkBlackList": true,
@@ -13,6 +14,7 @@
         "service2": {
             "serviceType": "find",
             "source": "file:///tmp/amlo.mp4",
+            "cameraId": "camera2_mac_address",
             "enabled": true,
             "faceDbFile": "found_service2.dat",
             "checkBlackList": true,
@@ -25,6 +27,7 @@
         "service1": {
             "serviceType": "find",
             "source": "file:///tmp/amlo.mp4",
+            "cameraId": "camera1_mac_address",
             "enabled": false,
             "faceDbFile": "found_service1.dat",
             "checkBlackList": true,
@@ -33,18 +36,26 @@
             "saveFacesDb": true
             },
         "service2": {
-            "serviceType": "recurrence",
+            "serviceType": "blackList",
             "source": "file:///tmp/amlo.mp4",
-            "enabled": true,
-            "faceDbFile": "recurrence_service2.dat",
-            "checkBlackList": true,
-            "checkWhieteList": true
+            "cameraId": "camera2_mac_address",
+            "enabled": true
             },
         "service3": {
             "serviceType": "ageAndGender",
             "source": "file:///tmp/amlo.mp4",
+            "cameraId": "camera3_mac_address",
             "enabled": false,
             "faceDbFile": "age_and_gender_service3.dat"
+            },
+        "service4": {
+            "serviceType": "recurrence",
+            "source": "file:///tmp/amlo.mp4",
+            "cameraId": "camera2_mac_address",
+            "enabled": false,
+            "faceDbFile": "recurrence_service2.dat",
+            "checkBlackList": true,
+            "checkWhiteList": true
             }
         }
 }
