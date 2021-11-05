2e76d2abd34608f5e172689ee8cddae9a5ab7ef2
Edgar Ramirez <edgcramirez@gmail.com>
2021-11-04 20:54:08 -0600

    new testing config

--- apps/deepstream-imagedata-multistream/configs/Server_Emulatation_configs_from_Excel.py
+++ apps/deepstream-imagedata-multistream/configs/Server_Emulatation_configs_from_Excel.py
@@ -23,39 +23,38 @@
         },
     "9c:7b:ef:2a:b6:07" : {
         "camera11_mac_address": {
-            "serviceType": "find",
-            "source": "file:///tmp/amlo.mp4",
-            "enabled": true,
-            "generalFaceDectDbFile": "found_service1.dat",
-            "checkBlackList": true,
-            "checkWhiteList": true,
-            "ignorePreviousDb": true,
-            "saveFacesDb": false 
-            },
-        "camera12_mac_address": {
-            "serviceType": "blackList",
-            "source": "file:///tmp/amlo.mp4",
-            "enabled": false 
-            },
-        "camera13_mac_address": {
-            "serviceType": "ageAndGender",
-            "source": "file:///tmp/amlo.mp4",
-            "enabled": false,
-            "generalFaceDectDbFile": "age_and_gender_service3.dat"
+            "find": {
+                "source": "file:///tmp/amlo.mp4",
+                "enabled": false,
+                "generalFaceDectDbFile": "found_service1.dat",
+                "checkBlackList": true,
+                "checkWhiteList": true,
+                "ignorePreviousDb": true,
+                "saveFacesDb": false 
+                },
+            "blackList": {
+                "source": "rtsp://192.168.129.14:9000/live",
+                "enabled": true
+                },
+            "ageAndGender": {
+                "source": "file:///tmp/amlo.mp4",
+                "enabled": false,
+                "generalFaceDectDbFile": "age_and_gender_service3.dat"
+                },
+            "recurrence": {
+                "source": "file:///tmp/amlo.mp4",
+                "cameraId": "camera2_mac_address ESTE SERVICIO FALLA YA QUE EJECUTA BLACKLIST instead of recurrence",
+                "enabled": false,
+                "checkBlackList": false,
+                "blacklistDbFile": "edgarblack",
+                "checkWhiteList": false
+                }
             },
         "camera14_mac_address": {
-            "serviceType": "recurrence",
-            "source": "file:///tmp/amlo.mp4",
-            "cameraId": "camera2_mac_address ESTE SERVICIO FALLA YA QUE EJECUTA BLACKLIST instead of recurrence",
-            "enabled": false,
-            "checkBlackList": false,
-            "blacklistDbFile": "edgarblack",
-            "checkWhiteList": false
-            },
-        "service6": {
-            "serviceType": "blackList",
-            "source": "file:///tmp/amlo.mp4",
-            "enabled": false
+            "blackList": {
+                "source": "file:///tmp/amlo.mp4",
+                "enabled": false
+                }
             }
         }
 }
